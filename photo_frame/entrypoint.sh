66f931d01397f34e74a34b1bbca32263f87b81ad
petterbejo <petterbj@outlook.dk>
2023-10-20 09:21:17 +0200

    Remove mouse pointer

--- photo_frame/entrypoint.sh
+++ photo_frame/entrypoint.sh
@@ -1,2 +1,4 @@
-sudo apt install feh 
-DISPLAY=:0.0 feh --randomize --full-screen --slideshow-delay 30 --auto-rotate $HOME/Pictures
+sudo apt install feh unclutter
+DISPLAY=:0.0 feh --randomize --full-screen --slideshow-delay 30 --auto-rotate $HOME/Pictures &
+mkdir -p $HOME/config/lxsession/LXDE-pi
+echo "@unclutter -idle 0" > ~/.config/lxsession/LXDE-pi/autostart
