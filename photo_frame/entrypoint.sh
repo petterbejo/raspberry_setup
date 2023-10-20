6430831d0e0316ecef9e9f48354e27d813b1eb44
petterbejo <petterbj@outlook.dk>
2023-10-20 09:48:06 +0200

    Correct creation of directory and file

--- photo_frame/entrypoint.sh
+++ photo_frame/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt install feh unclutter
 DISPLAY=:0.0 feh --randomize --full-screen --slideshow-delay 30 --auto-rotate ~/Pictures &
-mkdir -p ~/config/lxsession/LXDE-pi
+de_settings_dir=~/config/lxsession/LXDE-pi
+mkdir -p $de_settings_dir
+touch $de_settings_dir/autostart
 echo "@unclutter -idle 0" > ~/.config/lxsession/LXDE-pi/autostart
