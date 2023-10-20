103334b3e35bdbc13df828a6750ab9a4a13b21e5
petterbejo <petterbj@outlook.dk>
2023-10-20 14:14:44 +0200

    Retry disable mouse pointer

--- photo_frame/entrypoint.sh
+++ photo_frame/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt install feh unclutter
 DISPLAY=:0.0 feh --randomize --full-screen --slideshow-delay 30 --auto-rotate ~/Pictures &
-echo "@unclutter -idle 0 -root" >> /etc/xdg/lxsession/LXDE/autostart"
+echo "@unclutter -idle 0 -root" >> "/etc/xdg/lxsession/LXDE/autostart"
