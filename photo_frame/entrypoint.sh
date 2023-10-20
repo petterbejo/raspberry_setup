ec93e8729a3958a38603d1ab19579d7d68fe4615
petterbejo <petterbj@outlook.dk>
2023-10-20 09:49:09 +0200

    Try correct error

--- photo_frame/entrypoint.sh
+++ photo_frame/entrypoint.sh
@@ -4,4 +4,4 @@ DISPLAY=:0.0 feh --randomize --full-screen --slideshow-delay 30 --auto-rotate ~/
 de_settings_dir=~/config/lxsession/LXDE-pi
 mkdir -p $de_settings_dir
 touch $de_settings_dir/autostart
-echo "@unclutter -idle 0" > ~/.config/lxsession/LXDE-pi/autostart
+echo "@unclutter -idle 0" >> ~/.config/lxsession/LXDE-pi/autostart
