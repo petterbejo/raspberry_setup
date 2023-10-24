4b2539f16ca4691377dfef0c9b934a4b3b485abb
petterbejo <petterbj@outlook.dk>
2023-10-24 08:39:54 +0200

    Remove unclutter

--- photo_frame/entrypoint.sh
+++ photo_frame/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo apt install feh unclutter
+sudo apt install feh 
 DISPLAY=:0.0 feh --randomize --full-screen --slideshow-delay 30 --auto-rotate ~/Pictures &
-sudo echo "@unclutter -idle 0 -root" >> "/etc/xdg/lxsession/LXDE/autostart"
-sudo echo "@unclutter -idle 0 -root" >> "/etc/xdg/lxsession/LXDE-pi/autostart"
