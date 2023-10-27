aee5807d0f30c899067eab6877e29c2199224b10
petterbejo <petterbj@outlook.dk>
2023-10-27 13:28:09 +0200

    Correct photo app errors

--- photo_frame/entrypoint.sh
+++ photo_frame/entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 sudo apt install feh 
-DISPLAY=:0.0 feh --randomize --full-screen --slideshow-delay 30 --auto-rotate ~/Pictures &
---
+++ photo_frame/run_photo_display.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+DISPLAY=:0.0 feh --randomize --full-screen --slideshow-delay 75 --auto-rotate ~/Pictures &
